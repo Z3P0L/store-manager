8f9771472695728c06ac494c8a474bf20214326c
Z3P0L <worldtico@gmail.com>
2023-04-26 21:34:06 -0500

    add design

--- Vistas/frmMain.Designer.cs
+++ Vistas/frmMain.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblBienvenido = new System.Windows.Forms.Label();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            this.lblBienvenido.AutoSize = true;
+            this.lblBienvenido.Location = new System.Drawing.Point(12, 9);
+            this.lblBienvenido.Name = "lblBienvenido";
+            this.lblBienvenido.Size = new System.Drawing.Size(71, 13);
+            this.lblBienvenido.TabIndex = 0;
+            this.lblBienvenido.Text = "Bienvenido/a";
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(78, 36);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(101, 13);
+            this.lblHeader.TabIndex = 1;
+            this.lblHeader.Text = "¿Qué desea hacer?";
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(16, 72);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(103, 45);
+            this.btnRegistro.TabIndex = 2;
+            this.btnRegistro.Text = "Registrarse";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.Location = new System.Drawing.Point(125, 72);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(103, 45);
+            this.btnPagar.TabIndex = 3;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmMain";
+            this.ClientSize = new System.Drawing.Size(256, 143);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.lblBienvenido);
+            this.Name = "frmMain";
+            this.Text = "Interfaz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblBienvenido;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.Button btnPagar;
     }
 }
