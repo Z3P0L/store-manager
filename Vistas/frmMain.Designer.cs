f7a00d58512b6b3e50719931c1454df310c79f41
Z3P0L <worldtico@gmail.com>
2023-05-02 17:02:06 -0500

    redesign frmMain.Designer.cs

--- Vistas/frmMain.Designer.cs
+++ Vistas/frmMain.Designer.cs
@@ -28,35 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblBienvenido = new System.Windows.Forms.Label();
             this.lblHeader = new System.Windows.Forms.Label();
             this.btnCaja = new System.Windows.Forms.Button();
             this.btnUsuarios = new System.Windows.Forms.Button();
+            this.llblReporsitory = new System.Windows.Forms.LinkLabel();
+            this.btnProductos = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lblBienvenido
-            // 
-            this.lblBienvenido.AutoSize = true;
-            this.lblBienvenido.Location = new System.Drawing.Point(12, 9);
-            this.lblBienvenido.Name = "lblBienvenido";
-            this.lblBienvenido.Size = new System.Drawing.Size(71, 13);
-            this.lblBienvenido.TabIndex = 0;
-            this.lblBienvenido.Text = "Bienvenido/a";
             // 
             // lblHeader
             // 
             this.lblHeader.AutoSize = true;
-            this.lblHeader.Location = new System.Drawing.Point(78, 36);
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(72, 31);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(101, 13);
+            this.lblHeader.Size = new System.Drawing.Size(115, 20);
             this.lblHeader.TabIndex = 1;
-            this.lblHeader.Text = "¿Qué desea hacer?";
+            this.lblHeader.Text = "Store manager";
             // 
             // btnCaja
             // 
-            this.btnCaja.Location = new System.Drawing.Point(125, 72);
+            this.btnCaja.Location = new System.Drawing.Point(16, 106);
             this.btnCaja.Name = "btnCaja";
-            this.btnCaja.Size = new System.Drawing.Size(103, 45);
+            this.btnCaja.Size = new System.Drawing.Size(227, 45);
             this.btnCaja.TabIndex = 3;
             this.btnCaja.Text = "Ir a caja";
             this.btnCaja.UseVisualStyleBackColor = true;
@@ -66,20 +59,40 @@
             // 
             this.btnUsuarios.Location = new System.Drawing.Point(16, 72);
             this.btnUsuarios.Name = "btnUsuarios";
-            this.btnUsuarios.Size = new System.Drawing.Size(103, 45);
+            this.btnUsuarios.Size = new System.Drawing.Size(103, 28);
             this.btnUsuarios.TabIndex = 4;
             this.btnUsuarios.Text = "Usuarios";
             this.btnUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // llblReporsitory
+            // 
+            this.llblReporsitory.AutoSize = true;
+            this.llblReporsitory.Location = new System.Drawing.Point(16, 13);
+            this.llblReporsitory.Name = "llblReporsitory";
+            this.llblReporsitory.Size = new System.Drawing.Size(204, 13);
+            this.llblReporsitory.TabIndex = 5;
+            this.llblReporsitory.TabStop = true;
+            this.llblReporsitory.Text = "https://github.com/Z3P0L/store-manager";
+            // 
+            // btnProductos
+            // 
+            this.btnProductos.Location = new System.Drawing.Point(140, 72);
+            this.btnProductos.Name = "btnProductos";
+            this.btnProductos.Size = new System.Drawing.Size(103, 28);
+            this.btnProductos.TabIndex = 6;
+            this.btnProductos.Text = "Productos";
+            this.btnProductos.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(256, 143);
+            this.ClientSize = new System.Drawing.Size(255, 172);
+            this.Controls.Add(this.btnProductos);
+            this.Controls.Add(this.llblReporsitory);
             this.Controls.Add(this.btnUsuarios);
             this.Controls.Add(this.btnCaja);
             this.Controls.Add(this.lblHeader);
-            this.Controls.Add(this.lblBienvenido);
             this.Name = "frmMain";
             this.Text = "Interfaz";
             this.ResumeLayout(false);
@@ -88,10 +101,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblBienvenido;
         private System.Windows.Forms.Label lblHeader;
         private System.Windows.Forms.Button btnCaja;
         private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.LinkLabel llblReporsitory;
+        private System.Windows.Forms.Button btnProductos;
     }
 }
