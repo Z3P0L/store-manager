5296d89ef657fb9e8efc8f3d220fb9e98cb8c5e7
Z3P0L <worldtico@gmail.com>
2023-05-03 18:39:51 -0500

    add global id var

--- Clases/GlobalVars.cs
+++ Clases/GlobalVars.cs
@@ -8,5 +8,12 @@ namespace Proyecto_POO.Clases
 {
     internal class GlobalVars
     {
+        private static int _GlobalId;
+
+        public static int GlobalId
+        {
+            get { return _GlobalId; }
+            set { _GlobalId = value; }
+        }
     }
 }
