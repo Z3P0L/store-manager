773d303215e3ec79c33dd48e8f44b93cc92b69c3
Z3P0L <worldtico@gmail.com>
2023-05-03 18:44:20 -0500

    add db dependencies

--- Vistas/frmInventario.cs
+++ Vistas/frmInventario.cs
@@ -8,11 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Data.SqlClient;
+using Proyecto_POO.Clases;
 
 namespace Proyecto_POO.Vistas
 {
     public partial class frmInventario : Form
     {
+        database cn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataTable dt;
         public frmInventario()
         {
             InitializeComponent();
