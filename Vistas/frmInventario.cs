117330edf858ed36ebf96f42b96a3d98a4d77464
Z3P0L <worldtico@gmail.com>
2023-05-02 22:25:14 -0500

    add structure to productos datagridview

--- Vistas/frmInventario.cs
+++ Vistas/frmInventario.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Proyecto_POO.Vistas
 {
@@ -15,6 +16,24 @@ namespace Proyecto_POO.Vistas
         public frmInventario()
         {
             InitializeComponent();
+
+            dgvProductos.Columns.Add("Nombre", "Nombre");
+            dgvProductos.Columns.Add(new DataGridViewImageColumn() { Name = "Imagen", HeaderText = "Imagen", ImageLayout = DataGridViewImageCellLayout.Stretch });
+            dgvProductos.Columns.Add("Cantidad", "Cantidad");
+            dgvProductos.Columns.Add("Precio", "Precio");
+            dgvProductos.Columns.Add(new DataGridViewButtonColumn() { Name = "Editar", HeaderText = "Editar" });
+            dgvProductos.Columns.Add(new DataGridViewButtonColumn() { Name = "Eliminar", HeaderText = "Eliminar" });
+
+            dgvProductos.AllowUserToAddRows = false;
+            dgvProductos.ReadOnly = true;
+            dgvProductos.RowTemplate.Height = 100;
+
+            foreach (DataGridViewColumn col in dgvProductos.Columns)
+            {
+                col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+
+            dgvProductos.Columns["Imagen"].Width = 200;
         }
     }
 }
