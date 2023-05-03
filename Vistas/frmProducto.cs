2e50a952560111c3a5a9003aa856d213749a7bb9
Z3P0L <worldtico@gmail.com>
2023-05-03 09:08:33 -0500

    add database dependencies

--- Vistas/frmProducto.cs
+++ Vistas/frmProducto.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Proyecto_POO.Clases;
 
 namespace Proyecto_POO.Vistas
 {
     public partial class frmProducto : Form
     {
+        database cn;
+        SqlCommand cmd;
         string imgRoute = "";
         public frmProducto()
         {
@@ -30,7 +34,7 @@ namespace Proyecto_POO.Vistas
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btnVolver_Click(object sender, EventArgs e)
