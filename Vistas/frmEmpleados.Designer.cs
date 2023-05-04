bd3d3f3616c86982c4edb028f01a96f801af0187
Z3P0L <worldtico@gmail.com>
2023-05-04 13:59:04 -0500

    add layout to form

--- Vistas/frmEmpleados.Designer.cs
+++ Vistas/frmEmpleados.Designer.cs
@@ -28,19 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.btnNuevoEmpleado = new System.Windows.Forms.Button();
+            this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(318, 21);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(173, 24);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "Lista de empleados";
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(666, 12);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(122, 46);
+            this.btnVolver.TabIndex = 5;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevoEmpleado
+            // 
+            this.btnNuevoEmpleado.Location = new System.Drawing.Point(12, 12);
+            this.btnNuevoEmpleado.Name = "btnNuevoEmpleado";
+            this.btnNuevoEmpleado.Size = new System.Drawing.Size(122, 46);
+            this.btnNuevoEmpleado.TabIndex = 4;
+            this.btnNuevoEmpleado.Text = "Nuevo empleado";
+            this.btnNuevoEmpleado.UseVisualStyleBackColor = true;
+            // 
+            // dgvEmpleados
+            // 
+            this.dgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleados.Location = new System.Drawing.Point(12, 82);
+            this.dgvEmpleados.Name = "dgvEmpleados";
+            this.dgvEmpleados.Size = new System.Drawing.Size(776, 356);
+            this.dgvEmpleados.TabIndex = 7;
             // 
             // frmEmpleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvEmpleados);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnNuevoEmpleado);
             this.Name = "frmEmpleados";
             this.Text = "Empleados";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Button btnNuevoEmpleado;
+        private System.Windows.Forms.DataGridView dgvEmpleados;
     }
 }
