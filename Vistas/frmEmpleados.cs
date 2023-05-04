97bc8ad51734cbb711f52274b0ef8f615545abe4
Z3P0L <worldtico@gmail.com>
2023-05-04 16:20:31 -0500

    load users when form starts

--- Vistas/frmEmpleados.cs
+++ Vistas/frmEmpleados.cs
@@ -19,6 +19,8 @@ namespace Proyecto_POO.Vistas
         {
             InitializeComponent();
             dataGridViewDesign();
+
+            LoadUsuarios();
         }
 
         private void btnNuevoEmpleado_Click(object sender, EventArgs e)
