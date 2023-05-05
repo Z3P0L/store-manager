073702a242624ac51acfe975a60ac675c788f4d9
Z3P0L <worldtico@gmail.com>
2023-05-04 20:46:02 -0500

    add quantity field

--- Vistas/frmCaja.Designer.cs
+++ Vistas/frmCaja.Designer.cs
@@ -40,6 +40,8 @@ namespace Proyecto_POO.Vistas
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblTotal = new System.Windows.Forms.Label();
             this.btnPago = new System.Windows.Forms.Button();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -78,7 +80,7 @@ namespace Proyecto_POO.Vistas
             // 
             this.txtProductoId.Location = new System.Drawing.Point(112, 90);
             this.txtProductoId.Name = "txtProductoId";
-            this.txtProductoId.Size = new System.Drawing.Size(512, 20);
+            this.txtProductoId.Size = new System.Drawing.Size(384, 20);
             this.txtProductoId.TabIndex = 21;
             // 
             // btnAgregar
@@ -137,11 +139,30 @@ namespace Proyecto_POO.Vistas
             this.btnPago.Text = "Pago";
             this.btnPago.UseVisualStyleBackColor = true;
             // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidad.Location = new System.Drawing.Point(499, 91);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(61, 16);
+            this.lblCantidad.TabIndex = 27;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(566, 89);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(58, 20);
+            this.txtCantidad.TabIndex = 28;
+            // 
             // frmCaja
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.lblCantidad);
             this.Controls.Add(this.btnPago);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lblTotalTitle);
@@ -193,5 +214,7 @@ namespace Proyecto_POO.Vistas
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Button btnPago;
+        private Label lblCantidad;
+        private TextBox txtCantidad;
     }
 }
