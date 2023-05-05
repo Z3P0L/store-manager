84e9929d5c35b968a41170716b298d0fac2dd4f5
Z3P0L <worldtico@gmail.com>
2023-05-04 19:36:09 -0500

    add layout to form

--- Vistas/frmCaja.Designer.cs
+++ Vistas/frmCaja.Designer.cs
@@ -28,19 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblProductoId = new System.Windows.Forms.Label();
+            this.txtProductoId = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.lblTotalTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(345, 12);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(122, 46);
+            this.btnVolver.TabIndex = 18;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(12, 21);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(113, 24);
+            this.lblTitulo.TabIndex = 19;
+            this.lblTitulo.Text = "Bienvenid@";
+            // 
+            // lblProductoId
+            // 
+            this.lblProductoId.AutoSize = true;
+            this.lblProductoId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductoId.Location = new System.Drawing.Point(13, 90);
+            this.lblProductoId.Name = "lblProductoId";
+            this.lblProductoId.Size = new System.Drawing.Size(77, 16);
+            this.lblProductoId.TabIndex = 20;
+            this.lblProductoId.Text = "Producto ID";
+            // 
+            // txtProductoId
+            // 
+            this.txtProductoId.Location = new System.Drawing.Point(112, 90);
+            this.txtProductoId.Name = "txtProductoId";
+            this.txtProductoId.Size = new System.Drawing.Size(512, 20);
+            this.txtProductoId.TabIndex = 21;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(502, 12);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(122, 46);
+            this.btnAgregar.TabIndex = 22;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(16, 143);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(772, 295);
+            this.dgvProductos.TabIndex = 23;
+            // 
+            // lblTotalTitle
+            // 
+            this.lblTotalTitle.AutoSize = true;
+            this.lblTotalTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalTitle.Location = new System.Drawing.Point(690, 21);
+            this.lblTotalTitle.Name = "lblTotalTitle";
+            this.lblTotalTitle.Size = new System.Drawing.Size(60, 25);
+            this.lblTotalTitle.TabIndex = 24;
+            this.lblTotalTitle.Text = "Total";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTotal);
+            this.panel1.Location = new System.Drawing.Point(653, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 88);
+            this.panel1.TabIndex = 25;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(0, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(135, 88);
+            this.lblTotal.TabIndex = 0;
+            this.lblTotal.Text = "0";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmCaja
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTotalTitle);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtProductoId);
+            this.Controls.Add(this.lblProductoId);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnVolver);
             this.Name = "frmCaja";
             this.Text = "Caja";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblProductoId;
+        private System.Windows.Forms.TextBox txtProductoId;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Label lblTotalTitle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
